5bb132f3d8ae7cfd507d65c103d2fb329f5b3196
Touten3D-PC\Touten3D <sebpiret@hotmail.com>
2018-01-18 21:08:48 +0100

    - Creating a custom actor component

---
+++ Source/CodingTest/RandomMovementComp.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RandomMovementComp.h"
+
+
+// Sets default values for this component's properties
+URandomMovementComp::URandomMovementComp()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	movementRadius = 5;
+
+}
+
+
+// Called when the game starts
+void URandomMovementComp::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+
+// Called every frame
+void URandomMovementComp::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	AActor* parent = GetOwner();
+	if (parent)
+	{
+		parent->SetActorLocation(
+			parent->GetActorLocation() +
+			FVector(
+				FMath::FRandRange(-1, 1)*movementRadius,
+				FMath::FRandRange(-1, 1)*movementRadius,
+				FMath::FRandRange(-1, 1)*movementRadius
+			)
+		);
+	}
+}
+
